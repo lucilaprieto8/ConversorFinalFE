87a12cf6e62aaa1571195f692481a052a771d436
lucilaprieto8 <lucilaprieto08@gmail.com>
2023-12-13 23:21:09 -0300

    navbar

--- src/app/core/components/tabs/tabs.component.ts
+++ src/app/core/components/tabs/tabs.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class TabsComponent {
 
+  pageIn = [false, false, false];
+  constructor(private router: Router){
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd){
+        console.log("evento", event)
+        switch (event.urlAfterRedirects){
+          case "/conversion":
+            this.pageIn = [false, true, false]
+            console.log("home")
+            break;
+          case "/subscription":
+            this.pageIn = [true, false, false]
+            console.log("subs")
+            break;
+          case "/historical":
+            this.pageIn = [false, false, true]
+            console.log("his")
+            break;
+        }
+      }
+    })
+  }
+    linker(link: string){
+        this.router.navigate([link])
+    }
 }
